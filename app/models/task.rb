645206e165a623ea95e160ad1f800f6da09beb60
kevindhanna <kevindhanna@gmail.com>
2019-11-01 20:06:51 +0000

    add tests for 4 week days

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,9 @@ class Task < ActiveRecord::Base
   has_one :light_group, primary_key: :light_group_id, foreign_key: :hue_id
 
   def self.pretty_days(binary)
-    return "Monday" if binary == 1 
-    'Monday, Wednesday'
+    return "Monday" if binary == '1' 
+    return "Tuesday" if binary == '10' 
+    return "Wednesday" if binary == '100' 
+    return "Thursday" if binary == '1000' 
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,18 +1,22 @@
 describe Task do
-  before :each do
+  it 'knows its light_group' do
     test_group = LightGroup.create(hue_id: 1, name: 'test_group')
     task = Task.create(light_group_id: test_group.hue_id, action: 'turn_off')
-  end
-  it 'knows its light_group' do
     expect(task.light_group).to eq test_group
   end
 
   describe 'pretty_days' do
     it 'returns Monday when passed 1' do
-      expect(Task.pretty_days(1)).to eq 'Monday'
+      expect(Task.pretty_days('1')).to eq 'Monday'
     end
-    it 'returns "Monday, Wednesday" when passed 5' do
-      expect(Task.pretty_days(5)).to eq 'Monday, Wednesday'
+    it 'returns "Tuesday" when passed 10' do
+      expect(Task.pretty_days('10')).to eq 'Tuesday'
+    end
+    it 'returns "Wednesday" when passed 100' do
+      expect(Task.pretty_days('100')).to eq 'Wednesday'
+    end
+    it 'returns "Thursday" when passed 1000' do
+      expect(Task.pretty_days('1000')).to eq 'Thursday'
     end
   end
 end
