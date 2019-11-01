71fb7dbb5277e08ca43dfb28ddc0442d8ee64481
kevindhanna <kevindhanna@gmail.com>
2019-11-01 18:34:32 +0000

    add association from task to light group

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ActiveRecord::Base
-  has_one :light_group
+  has_one :light_group, primary_key: :group_id, foreign_key: :hue_id
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,2 +1,7 @@
 describe Task do
+  it 'knows its light_group' do
+    test_group = LightGroup.create(name: 'test_group', hue_id: 1)
+    task = Task.create(group_id: test_group.hue_id, action: 'turn_off')
+    expect(task.light_group).to eq test_group
+  end
 end
