a0d2f40e7bf8d9626284a8b2d288cea3d494b63f
kevindhanna <kevindhanna@gmail.com>
2019-11-01 19:54:54 +0000

    add pretty_days to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
   has_one :light_group, primary_key: :light_group_id, foreign_key: :hue_id
+
+  def self.pretty_days(binary)
+    "Monday"
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,7 +1,15 @@
 describe Task do
-  it 'knows its light_group' do
+  before :each do
     test_group = LightGroup.create(hue_id: 1, name: 'test_group')
     task = Task.create(light_group_id: test_group.hue_id, action: 'turn_off')
+  end
+  it 'knows its light_group' do
     expect(task.light_group).to eq test_group
+  end
+
+  describe 'pretty_days' do
+    it 'returns a string of days for a given binary list' do
+      expect(Task.pretty_days(1)).to eq 'Monday'
+    end
   end
 end
