437db699b31239f883fdea3d9bd0bc266282007d
kevindhanna <kevindhanna@gmail.com>
2019-11-01 17:56:02 +0000

    add update_groups class method to LightGroup

--- app/models/light_group.rb
+++ app/models/light_group.rb
@@ -1,2 +1,15 @@
 class LightGroup < ActiveRecord::Base
+  def self.update_groups
+    self.hue_client.groups.each do |group|
+      unless self.find_by(hue_id: group.id)
+        self.create(name: group.name, hue_id: group.id)
+      end
+    end
+  end
+
+  private 
+
+  def self.hue_client
+    @hue_client ||= Hue::Client.new
+  end
 end
--- spec/light_group_spec.rb
+++ spec/light_group_spec.rb
@@ -1,9 +1,20 @@
 describe LightGroup do
+  let(:hue_group2) { double(:hue_group, name: 'test2', id: 2) }
+  let(:hue_group) { double(:hue_group, name: 'test', id: 1) }
+  let(:hue_client) { double(:hue_client, groups: [hue_group, hue_group2]) }
   subject(:light_group) { LightGroup.create(name: 'test', hue_id: 1) }
   it 'knows its name' do
     expect(light_group.name).to eq 'test'
   end
   it 'knows its hue id' do
     expect(light_group.hue_id).to eq 1
+  end
+  describe '.update_groups' do
+    it 'updates the groups from the Hue hub' do
+      allow(LightGroup).to receive(:hue_client) { hue_client }
+      expect(hue_client).to receive(:groups)
+      LightGroup.update_groups
+      expect(LightGroup.find_by(hue_id: 2).name).to eq 'test2'
+    end
   end
 end
